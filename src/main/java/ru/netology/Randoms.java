86bfc55e0b4f37437283630924243380f6439493
Rishat Suleymanov <spk_irbis@mail.ru>
2021-02-04 23:02:32 +0300

    Added file Randoms.java

---
+++ src/main/java/ru/netology/Randoms.java
@@ -0,0 +1,19 @@
+package ru.netology;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+
+    protected Random random;
+
+    public Randoms(int min, int max) {
+        // TODO
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        // TODO
+        return null;
+    }
+}
